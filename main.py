caffe16acdd034d2bc71623b101d61018f89d537
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-06-17 18:26:03 +0300

    Year

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import datetime
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+event1 = datetime.datetime.now().year
+delta = event1-1919
+
+rendered_page = template.render(
+    time_years =delta
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
+
+
+# from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+
+# server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+# server.serve_forever()
