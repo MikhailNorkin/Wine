69d297fc8eb6e3997ceac8566f614774888317a4
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-06-18 23:52:32 +0300

    Done

--- main.py
+++ main.py
@@ -3,28 +3,22 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
+import collections
+from pprint import pprint
 
-excel_data_df1 = pandas.read_excel('wine.xlsx',sheet_name='Лист1')
-name_wine = excel_data_df1['Название'].tolist()
-excel_data_df2 = pandas.read_excel('wine2.xlsx')
-kat_wine = list(set(excel_data_df2['Категория'].tolist()))
-# excel_data_df3 = pandas.read_excel('wine2.xlsx', sheet_name='Лист1', usecols=['Категория', 'Название'])
-excel_data_df = pandas.read_excel('wine2.xlsx', sheet_name='Лист1')
-df = excel_data_df.to_dict(orient='record')
-for i in kat_wine:
-    print(i)
-    for j in df:
-        if j['Категория'] == i:
-            print(j)
+excel_wine = pandas.read_excel('wine.xlsx',sheet_name='Лист1')
+name_wine = excel_wine['Название'].tolist()
 
-# print(excel_data_df3)
+excel_kat = pandas.read_excel('wine3.xlsx')
+kat_wine = list(set(excel_kat['Категория'].tolist()))
+excel_data_df = pandas.read_excel('wine3.xlsx', sheet_name='Лист1',na_values=['N/A', 'NA'], keep_default_na=False)
+data_shot = excel_data_df.to_dict(orient='record')
+dict_of_lists = collections.defaultdict(list)
 
+for i in data_shot:
+    dict_of_lists[i['Категория']].append(i)
 
-# rec = excel_data_df3.to_dict(orient='record')
-# print(rec)
-# for i in excel_data_df3:
-#     print([i])
-
+pprint(dict_of_lists)
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -46,7 +40,8 @@ else:
 rendered_page = template.render(
     time_years = time_years,
     time_word = time_word,
-    name_wine = name_wine
+    name_wine = name_wine,
+    list_wine = dict_of_lists
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
@@ -54,10 +49,3 @@ with open('index.html', 'w', encoding="utf8") as file:
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
-
-
-# from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-
-# server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-# server.serve_forever()
