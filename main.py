8fe1e11d45bebcb40cac53900513daa15e38ae01
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-06-18 15:12:37 +0300

    Kat_win

--- main.py
+++ main.py
@@ -2,6 +2,29 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
+import pandas
+
+excel_data_df1 = pandas.read_excel('wine.xlsx',sheet_name='Лист1')
+name_wine = excel_data_df1['Название'].tolist()
+excel_data_df2 = pandas.read_excel('wine2.xlsx')
+kat_wine = list(set(excel_data_df2['Категория'].tolist()))
+# excel_data_df3 = pandas.read_excel('wine2.xlsx', sheet_name='Лист1', usecols=['Категория', 'Название'])
+excel_data_df = pandas.read_excel('wine2.xlsx', sheet_name='Лист1')
+df = excel_data_df.to_dict(orient='record')
+for i in kat_wine:
+    print(i)
+    for j in df:
+        if j['Категория'] == i:
+            print(j)
+
+# print(excel_data_df3)
+
+
+# rec = excel_data_df3.to_dict(orient='record')
+# print(rec)
+# for i in excel_data_df3:
+#     print([i])
+
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -22,7 +45,8 @@ else:
 
 rendered_page = template.render(
     time_years = time_years,
-    time_word = time_word
+    time_word = time_word,
+    name_wine = name_wine
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
