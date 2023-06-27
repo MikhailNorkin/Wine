6be02b71c3c8e94de78f9a3490d31ab9c8843a56
MikhailNorkin <109146379+MikhailNorkin@users.noreply.github.com>
2023-06-27 21:52:56 +0300

    Add def get_time_name, get_dict_of_lists

--- main.py
+++ main.py
@@ -8,27 +8,41 @@ import sys
 from pprint import pprint
 
 
+def get_time_name(time_years):
+    if time_years % 10 == 1:
+        time_word = "год"
+    elif (time_years % 10 >= 2) and (time_years % 10 <= 4):
+        time_word = "года"    
+    else:
+        time_word = "лет"
+    return time_word
+
+
+def get_dict_of_lists(name_excel):
+    excel_data_df = pandas.read_excel(name_excel, sheet_name='Лист1',na_values=['N/A', 'NA'], keep_default_na=False)
+    data_shot = excel_data_df.to_dict('record')
+    dict_of_lists = collections.defaultdict(list)
+    for i in data_shot:
+        dict_of_lists[i['Категория']].append(i)
+    return dict_of_lists
+
+
 def main():
     parser = argparse.ArgumentParser(
         description='Введите путь к файлу excel'
     )
     parser.add_argument('name', help='Путь к файлу')
     args = parser.parse_args()
+
+    name_excel = args.name
     try:
-        excel_wine = pandas.read_excel(args.name,sheet_name='Лист1')
+        excel_wine = pandas.read_excel(name_excel,sheet_name='Лист1')
         name_wines = excel_wine['Название'].tolist()
     except:
         print('Вы ввели неверное имя файла. Запустите программу заново')    
         sys.exit()
 
-    excel_kat = pandas.read_excel('wine3.xlsx')
-    category_wines = list(set(excel_kat['Категория'].tolist()))
-    excel_data_df = pandas.read_excel('wine3.xlsx', sheet_name='Лист1',na_values=['N/A', 'NA'], keep_default_na=False)
-    data_shot = excel_data_df.to_dict('record')
-    dict_of_lists = collections.defaultdict(list)
-
-    for i in data_shot:
-        dict_of_lists[i['Категория']].append(i)
+    dict_of_lists = get_dict_of_lists(name_excel)
 
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -40,12 +54,7 @@ def main():
     event1 = datetime.datetime.now().year
     time_years = event1-1902
 
-    if time_years % 10 == 1:
-        time_word = "год"
-    elif (time_years % 10 >= 2) and (time_years % 10 <= 4):
-        time_word = "года"    
-    else:
-        time_word = "лет"
+    time_word = get_time_name(time_years)
 
     rendered_page = template.render(
         time_years = time_years,
